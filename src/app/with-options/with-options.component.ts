6e52216482f7a47487e764b2f67ea1c1e08595dc
Uma Mahesh <uma.kondapally@gmail.com>
2018-03-31 16:27:44 +0530

    Data table

---
+++ src/app/with-options/with-options.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-with-options',
+  templateUrl: './with-options.component.html',
+  styleUrls: ['./with-options.component.css']
+})
+export class WithOptionsComponent implements OnInit {
+  dtOptions: DataTables.Settings = {};
+
+  constructor() { }
+
+  ngOnInit() {
+    this.dtOptions = {
+      pagingType: 'full_numbers'
+    };
+
+  }
+
+}
